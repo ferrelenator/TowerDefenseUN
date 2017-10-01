6fec42b5ef7c55240abd06bf5dd7a30f8038312b
daguic <32375751+daguic@users.noreply.github.com>
2017-10-01 12:07:12 -0500

    Agregados TODOS Cambiadas las variables de inicio por floats("Es para que el enemigo se mueva en el tablero y no se pase de entero a entero").
    Chicos por favor vayan adelantando todo lo que puedan en los TODOS
     si se les ocurre otros TODOS copienlos y hablen por el WHATSAPP
    digan en lo que estan trabajando para que dos no hagan lo mismo.

--- TowerDefense1/src/TD/business/Boot.java
+++ TowerDefense1/src/TD/business/Boot.java
@@ -1,3 +1,14 @@
+/*
+TODO:
+
+- Menu principal : un menu principal que permita Iniciar un nuevo juego, salir del juego 
+- Crear el "tiempo"
+- Pausar el tiempo o adelantarlo
+- Crear Oleadas y manejo de oleadas
+
+*/
+
+
 package TD.business;
 
 import TD.data.*;
@@ -15,12 +26,22 @@ public class Boot  {
     }
 
     private static void startGame() {
-
-    board = new Board();
+        int[][] map ={
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {1,1,1,1,0,0,0,0,0,0},
+            {1,1,1,1,0,0,0,0,0,0},
+            {0,0,1,1,0,0,0,0,0,0},
+            {0,0,1,1,0,0,0,0,0,0},
+            {0,0,1,1,1,1,1,1,1,1},
+            {0,0,1,1,1,1,1,1,1,1},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+        };
+    board = new Board(map);
     tower = new Tower(1,1);
     enemy = new Enemy(2,2);
     UI.printWelcome();
-    board.road();
     tower.drawn(board);
     enemy.drawn(board);
     UI.printBoard(board);
